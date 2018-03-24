cc1176f8cc5be2497fcd29b28b5dd21e8ffbffc6
The-DarkBeast <pranaynarang@gmail.com>
2018-03-24 14:20:54 +0100

    Revert "lineage-sdk: Update the way OMS records details about overlays"
    This reverts commit 8638e87d778822c2cb3ee2db8982161b6c00be7d.
    
    Change-Id: I7d3eb1d3451c795f34f85209ca771d277503f50a

--- lineage/lib/main/java/org/lineageos/platform/internal/StyleInterfaceService.java
+++ lineage/lib/main/java/org/lineageos/platform/internal/StyleInterfaceService.java
@@ -198,8 +198,7 @@ public class StyleInterfaceService extends LineageSystemService {
 
         try {
             PackageInfo pi = mPackageManager.getPackageInfo(pkgName, 0);
-            return pi != null && (pi.overlayFlags & PackageInfo.FLAG_OVERLAY_STATIC) == 0 &&
-                    isValidAccent(pkgName);
+            return pi != null && !pi.isStaticOverlay && isValidAccent(pkgName);
         } catch (PackageManager.NameNotFoundException e) {
             return false;
         }
